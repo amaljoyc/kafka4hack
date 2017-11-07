3868131d2fb03128ec9ca598258e43364e768794
Amal Joy Chemparathy <amaljoyc@gmail.com>
2017-11-08 00:21:48 +0100

    init

---
+++ src/main/java/com/amaljoyc/kafka4hack/Kafka4hackApplication.java
@@ -0,0 +1,12 @@
+package com.amaljoyc.kafka4hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka4hackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafka4hackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/amaljoyc/kafka4hack/Kafka4hackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amaljoyc.kafka4hack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kafka4hackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
